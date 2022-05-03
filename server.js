b25b6d4a7754ba6b9baac5e37a6946bacce30d25
arielmell0 <arielmello@rede.ulbra.br>
2022-05-03 03:17:29 -0300

    feat: conectado o nosso servidor a base de dados
    usando event emiters para mostrar os logs corretamente

--- server.js
+++ server.js
@@ -5,6 +5,15 @@ import mongoose from 'mongoose';
 const app = express();
 const port = 3030;
 
-app.listen(port, () => {
-    console.log(`O servidor está rodando em https://localhost:${port}`);
-});
+mongoose.connect(process.env.CONNECTIONSTRING)
+    .then(() => {
+        console.log('Banco de dados conectado.');
+        app.emit('databaseOn');
+    })
+    .catch(error => console.log('Erro ao conectar ao banco de dados: ', error)); 
+
+app.on('bdOn', () => {
+    app.listen(port, () => {
+        console.log(`O servidor está rodando em https://localhost:${port}`);
+    });
+})
